87478f6d862941ceaec86bb62783e52f32a71064
shifatmanjum <khshifat@gmail.com>
2022-09-09 09:43:53 +0200

    Love Propose

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("Yes").addEventListener("click", function () {
+    document.getElementById("Message").innerText = "I love You  "
+});
+document.getElementById("No").addEventListener("click", function () {
+    document.getElementById("Message").innerText = "You are a good friend only  "
+
+});
